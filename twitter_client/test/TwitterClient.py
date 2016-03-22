7e5eaa6e9647b4c0a0e58a501888a6b4e9334d29
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-03-22 23:58:14 +0000

    Connect to the Twitter API and get tweets' text.

--- twitter_client/test/TwitterClient.py
+++ twitter_client/test/TwitterClient.py
@@ -1,10 +1,30 @@
-import urllib, urllib2, json
+import urllib, urllib2, json, tweepy
 from pyquery import PyQuery
+from tweepy import OAuthHandler
+from tweepy.error import TweepError
 
 class TwitterClient:
 
-    def get_tweets(self, term):
+    def __init__(self):
+        self.api = self._get_twitter_api()
 
+    def _get_twitter_api(self):
+        """
+        Since we are only reading public information from Twitter, we don't need
+        access token/secret values.
+        """
+        with open('secrets.json') as secrets_file:
+            secrets = json.load(secrets_file)
+
+        consumer_key = secrets['consumer_key']
+        consumer_secret = secrets['consumer_secret']
+
+        auth = OAuthHandler(consumer_key, consumer_secret)
+
+        return tweepy.API(auth)
+
+
+    def get_tweet_ids(self, term):
         tweet_ids = []
 
         refreshCursor = ''
@@ -12,28 +32,54 @@ class TwitterClient:
         while True:
             response = self.getJsonReponse(term, refreshCursor)
             refreshCursor = response['min_position']
-            tweets = PyQuery(response['items_html'])('div.js-stream-tweet')
+
+            try:
+                tweets = PyQuery(response['items_html'])('div.js-stream-tweet')
+            except Exception:
+                break
+
+            if len(tweets) == 0:
+                break
+
+            if len(tweet_ids) >= 80:
+                break
 
             for tweetHTML in tweets:
                 tweetPQ = PyQuery(tweetHTML)
                 tweet_id = tweetPQ.attr("data-tweet-id")
                 tweet_ids.append(tweet_id)
 
-            break
-
-
-
-
-
-
         return tweet_ids
 
 
     def getJsonReponse(self, term, refreshCursor):
+        """
+        Prepare HTTP GET request to be sent to Twitter API and search for tweets
+        based on given term inside a limited min/max position.
+        """
         url = "https://twitter.com/i/search/timeline?f=realtime&q={}&src=typd&max_position={}".format(urllib.quote(term), refreshCursor)
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
-        # print "URL: " + url
         request = urllib2.Request(url, headers = headers)
-        jsonResponse = urllib2.urlopen(request).read()
-        dataJson = json.loads(jsonResponse)
-        return dataJson
+        response = urllib2.urlopen(request).read()
+        json_response = json.loads(response)
+        return json_response
+
+    def get_tweets(self, term):
+        """
+        Given a list of tweet IDs, return all information related to it.
+        """
+        tweet_ids = self.get_tweet_ids(term)
+
+        try:
+            tweets = self.api.statuses_lookup(tweet_ids)
+        except TweepError:
+            return []
+
+        for tweet in tweets:
+            tweet_info = self.get_tweet_info(tweet)
+
+        return tweets
+
+    def get_tweet_info(self, tweet):
+        tweet_info = ''
+        return tweet_info
