f2129052515b71e5a9eb860ab5c6470591ea554f
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-03-22 23:58:38 +0000

    Added a tweet for 0 results.

--- twitter_client/test/test_twitter_client.py
+++ twitter_client/test/test_twitter_client.py
@@ -5,9 +5,12 @@ class TestTwitterClient:
 
     def test_get_bioconductor_tweets(self):
         term = "#bioconductor"
-        # begin = "2006-01-01"
-        # end = "2016-03-23"
         client = TwitterClient()
-        # results = client.get_tweets(term, begin, end)
         results = client.get_tweets(term)
-        assert len(results) < 0
+        assert len(results) == 82
+
+    def test_get_empty_tweets(self):
+        term = "#mnhgjkugfdvgh9458345"
+        client = TwitterClient()
+        results = client.get_tweets(term)
+        assert len(results) == 0
