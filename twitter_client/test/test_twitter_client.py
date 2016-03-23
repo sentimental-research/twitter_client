71bce35f378fdd0d4f32ecb28b8f6a3c762a562d
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-03-23 01:35:51 +0000

    Added test for ReciPy.

--- twitter_client/test/test_twitter_client.py
+++ twitter_client/test/test_twitter_client.py
@@ -4,12 +4,27 @@ from TwitterClient import TwitterClient
 class TestTwitterClient:
 
     def test_get_bioconductor_tweets(self):
+        """
+        Check if we get the expected results for bioconductor.
+        """
         term = "#bioconductor"
         client = TwitterClient()
         results = client.get_tweets(term)
         assert len(results) == 82
 
+    def test_get_recipy_tweets(self):
+        """
+        Check if we get the expected behaviour when very few tweets returned.
+        """
+        term = "#recipy"
+        client = TwitterClient()
+        results = client.get_tweets(term)
+        assert len(results) == 0
+
     def test_get_empty_tweets(self):
+        """
+        Check if we get the expected behaviour when no tweets are returned.
+        """
         term = "#mnhgjkugfdvgh9458345"
         client = TwitterClient()
         results = client.get_tweets(term)
